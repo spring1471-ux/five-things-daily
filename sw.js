a258a798c31f724ed64c5ffeaa46bded4980a5f7
spring1471-ux <spring1471@126.com>
2026-09-10 10:55:25 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = "five-things-shell-v4";
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["/", "/favicon.svg"]))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).pathname.startsWith("/api/")) return;
+  event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request)));
+});
